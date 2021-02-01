934b1cf272ce2ddd55a23c6b732b21e253b6f7af
Entagon <karl-wenzel@gmx.de>
2021-02-01 18:38:09 +0100

    worked on FunctionSolver

--- Logistics/FunctionSolver.cs
+++ Logistics/FunctionSolver.cs
@@ -25,11 +25,23 @@ namespace StatsPlus
             }
         }
 
+        /// <summary>
+        /// Solves a simple literal. Numeric symbols and +,-,*,/ are allowed. If your literal contains variables such as a,x,myVar etc, consider using the overload SolveSimpleLiteral(string, VariableAssignments) to assign them to their values.
+        /// </summary>
+        /// <param name="Literal">The simple literal to solve.</param>
+        /// <param name="Assignments">Assign the variable values here.</param>
+        /// <returns>The result of the evaluated literal.</returns>
         public static float SolveSimpleLiteral(string Literal)
         {
             return SolveSimpleLiteral(Literal, new VariableAssignments());
         }
 
+        /// <summary>
+        /// Solves a simple literal. Numeric symbols and +,-,*,/ are allowed. If your literal contains variables such as a,x,myVar etc, you can replace them with the VariableAssignments-class.
+        /// </summary>
+        /// <param name="Literal">The simple literal to solve.</param>
+        /// <param name="Assignments">Assign the variable values here.</param>
+        /// <returns>The result of the evaluated literal.</returns>
         public static float SolveSimpleLiteral(string Literal, VariableAssignments Assignments)
         {
             foreach (SatisfyVariable variableAssignment in Assignments.Data)
@@ -37,8 +49,11 @@ namespace StatsPlus
                 Literal = Literal.Replace(variableAssignment.VariableName, variableAssignment.VariableValue+"");
             }
 
+            Literal = Literal.Replace("-", "+(-)").Replace("/", "*(/)");
+
             float Solve(string input)
             {
+
                 float result;
                 string[] sums = input.Split('+');
                 if (sums.Length != 1)
@@ -49,6 +64,11 @@ namespace StatsPlus
                         result = Solve(sums[i]) + result;
                     }
                     return result;
+                }
+
+                if (input.Length > 2 && input.Substring(0, 3).Equals("(-)"))
+                {
+                    return -1f * Solve(input.Remove(0, 3));
                 }
 
                 string[] factors = input.Split('*');
@@ -62,11 +82,26 @@ namespace StatsPlus
                     return result;
                 }
 
+                if (input.Length > 2 && input.Substring(0, 3).Equals("(/)"))
+                {
+                    return 1f / Solve(input.Remove(0, 3));
+                }
+
+                string[] exponential = input.Split('^');
+                if (exponential.Length != 1)
+                {
+                    result = Solve(exponential[0]);
+                    for (int i = 1; i < exponential.Length; i++)
+                    {
+                        result = Mathf.Pow(result, Solve(exponential[i]));
+                    }
+                    return result;
+                }
 
                 if (float.TryParse(input, out result)) return result;
                 else
                 {
-                    Debug.LogError("Could not parse symbol: <b>" + input + "</b>. Maybe your input " + Literal + " was formatted wrong?");
+                    Debug.LogError("Could not parse symbol: <b>" + input + "</b>. Value unknown. Maybe your input " + Literal + " was formatted wrong or you are missing variable assignments.");
                     return 0f;
                 }
             }
