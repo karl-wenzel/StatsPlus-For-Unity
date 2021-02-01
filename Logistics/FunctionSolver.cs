7d9834061acde0109b613c315eebfed25df844a9
Entagon <karl-wenzel@gmx.de>
2021-02-02 00:05:30 +0100

    worked on function solver

--- Logistics/FunctionSolver.cs
+++ Logistics/FunctionSolver.cs
@@ -26,7 +26,51 @@ namespace StatsPlus
         }
 
         /// <summary>
-        /// Solves a simple literal. Numeric symbols and +,-,*,/ are allowed. If your literal contains variables such as a,x,myVar etc, consider using the overload SolveSimpleLiteral(string, VariableAssignments) to assign them to their values.
+        /// use this to check if your literal parts that are not allowed and will result in unpredictable behaviour if you try to solve them.
+        /// </summary>
+        /// <param name="Literal"></param>
+        /// <returns></returns>
+        public static bool LiteralWellFormatted(string Literal)
+        {
+            if (Literal.Contains("[") || Literal.Contains("]"))
+            {
+                Debug.Log(Literal+" Format Info: [ and ] not allowed.");
+                return false;
+            }
+            if (Literal.Contains("--"))
+            {
+                Debug.Log(Literal+" Format Info: -- not allowed.");
+                return false;
+            }
+            if (Literal.Contains("-(-"))
+            {
+                Debug.Log(Literal+" Format Info: -(- not allowed.");
+                return false;
+            }
+            int OpenedBrackets = 0;
+            for (int i = 0; i < Literal.Length; i++)
+            {
+                if (Literal[i] == '(')
+                {
+                    OpenedBrackets++;
+                    continue;
+                }
+                if (Literal[i] == ')')
+                {
+                    OpenedBrackets--;
+                    continue;
+
+                }
+            }
+            if (OpenedBrackets > 0) {
+                Debug.LogError(Literal+" Format Info: Literal not well formatted. Couldn't close all brackets.");
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Solves a simple literal. Numeric symbols, brackets () and +,-,*,/ are allowed.
         /// </summary>
         /// <param name="Literal">The simple literal to solve.</param>
         /// <param name="Assignments">Assign the variable values here.</param>
@@ -37,7 +81,7 @@ namespace StatsPlus
         }
 
         /// <summary>
-        /// Solves a simple literal. Numeric symbols and +,-,*,/ are allowed. If your literal contains variables such as a,x,myVar etc, you can replace them with the VariableAssignments-class.
+        /// Solves a simple literal. Numeric symbols, brackets () and +,-,*,/ are allowed. If your literal contains variables such as a, x, myVar etc, you can replace them with the VariableAssignments-class.
         /// </summary>
         /// <param name="Literal">The simple literal to solve.</param>
         /// <param name="Assignments">Assign the variable values here.</param>
@@ -46,10 +90,40 @@ namespace StatsPlus
         {
             foreach (SatisfyVariable variableAssignment in Assignments.Data)
             {
-                Literal = Literal.Replace(variableAssignment.VariableName, variableAssignment.VariableValue+"");
+                Literal = Literal.Replace(variableAssignment.VariableName, variableAssignment.VariableValue + "");
             }
 
-            Literal = Literal.Replace("-", "+(-)").Replace("/", "*(/)");
+            Literal = Literal.Replace("-", "+[-]").Replace("/", "*[/]");
+
+
+            int OpenedBrackets = 0;
+            int LastOpenBracketIndex = 0;
+            for (int i = 0; i < Literal.Length; i++)
+            {
+                if (Literal[i] == '(')
+                {
+                    OpenedBrackets++;
+                    LastOpenBracketIndex = i;
+                    continue;
+                }
+                if (Literal[i] == ')')
+                {
+                    if (OpenedBrackets != 0)
+                    {
+                        float f = Solve(Literal.Substring(LastOpenBracketIndex + 1, i - (LastOpenBracketIndex + 1)));
+                        Literal = Literal.Remove(LastOpenBracketIndex, i - LastOpenBracketIndex + 1);
+                        Literal = Literal.Insert(LastOpenBracketIndex, f.ToString());
+                        OpenedBrackets = 0;
+                        LastOpenBracketIndex = 0;
+                        i = -1;
+                        continue;
+                    }
+                    else
+                    {
+                        Debug.LogError("Couldnt parse literal " + Literal + ". You are closing too many brackets.");
+                    }
+                }
+            }
 
             float Solve(string input)
             {
@@ -66,7 +140,7 @@ namespace StatsPlus
                     return result;
                 }
 
-                if (input.Length > 2 && input.Substring(0, 3).Equals("(-)"))
+                if (input.Length > 2 && input.Substring(0, 3).Equals("[-]"))
                 {
                     return -1f * Solve(input.Remove(0, 3));
                 }
@@ -82,7 +156,7 @@ namespace StatsPlus
                     return result;
                 }
 
-                if (input.Length > 2 && input.Substring(0, 3).Equals("(/)"))
+                if (input.Length > 2 && input.Substring(0, 3).Equals("[/]"))
                 {
                     return 1f / Solve(input.Remove(0, 3));
                 }
