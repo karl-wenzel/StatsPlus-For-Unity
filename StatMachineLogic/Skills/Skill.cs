6d1574ec6532800b99860cf8b7ed9b68933c51ea
Entagon <karl-wenzel@gmx.de>
2021-04-09 19:09:10 +0200

    added conditions to skill effects

--- StatMachineLogic/Skills/Skill.cs
+++ StatMachineLogic/Skills/Skill.cs
@@ -29,7 +29,9 @@ namespace StatsPlus
         /// <param name="strength">The strength modifier that will be passed to the effects.</param>
         /// <param name="statMachine">A link to the statMachine that ordered the processing.</param>
         /// <returns>The value of the stat after all skill effects have been applied.</returns>
-        public object ProcessStat(Stat stat, object value, float strength, StatMachine statMachine) {
+        public object ProcessStat(Stat stat, object value, float strength, StatMachine statMachine)
+        {
+            //evaluate the conditions in this skill
             foreach (Condition condition in Conditions)
             {
                 if (condition.SelfEvaluate(statMachine) == false) return value;
@@ -39,27 +41,51 @@ namespace StatsPlus
             {
                 if (effect.affectsStatName.Equals(stat.Identifier))
                 {
+                    //evaluate the conditions in the SkillEffect. If one condition is false, continue with next SkillEffect
+                    foreach (Condition condition in effect.Conditions)
+                    {
+                        if (condition.SelfEvaluate(statMachine) == false) continue;
+                    }
+
                     value = effect.ProcessStat(stat, value, effect.ignoreSkillStrength ? 1f : strength, statMachine);
                 }
             }
             return value;
         }
 
-        public Skill(string Name, params SkillEffect[] InitialEffects) : base(Name) {
+        public Skill(string Name, params SkillEffect[] InitialEffects) : base(Name)
+        {
             AddEffects(InitialEffects);
         }
 
-        public Skill AddConditions(params Condition[] conditions) {
-            Conditions.AddRange(conditions);
+        /// <summary>
+        /// Add a condition that must be resolved true for the skill to apply.
+        /// </summary>
+        /// <param name="Condition">The Condition that should be added</param>
+        public Skill AddCondition(Condition Condition)
+        {
+            Conditions.Add(Condition);
             return this;
         }
 
-        public Skill AddConditionEquals(string factId, object compareTo) {
+        /// <summary>
+        /// Add one or more conditions that must be resolved true for the skill to apply.
+        /// </summary>
+        /// <param name="Condition">The Conditions that should be added</param>
+        public Skill AddConditions(params Condition[] Conditions)
+        {
+            this.Conditions.AddRange(Conditions);
+            return this;
+        }
+
+        public Skill AddConditionEquals(string factId, object compareTo)
+        {
             Conditions.Add(new ConditionEquals(factId, compareTo));
             return this;
         }
 
-        public Skill AddEffect(SkillEffect newEffect) {
+        public Skill AddEffect(SkillEffect newEffect)
+        {
             Effects.Add(newEffect);
             return this;
         }
@@ -76,13 +102,15 @@ namespace StatsPlus
             {
                 Effects.RemoveAt(atIndex);
             }
-            else {
+            else
+            {
                 Debug.LogError("Couldn't remove effect at " + atIndex + " because the Skill only has " + Effects.Count + " effects.");
             }
             return this;
         }
 
-        public string PrettyPrint() {
+        public string PrettyPrint()
+        {
             string result = "Skill|name:" + Identifier + "|content:";
             foreach (SkillEffect effect in Effects)
             {
--- StatMachineLogic/Skills/SkillEffect.cs
+++ StatMachineLogic/Skills/SkillEffect.cs
@@ -13,14 +13,39 @@ namespace StatsPlus
         public string affectsStatName;
         public bool ignoreSkillStrength;
 
-        public SkillEffect(string affectsStatName, bool ignoreSkillStrength)
+        /// <summary>
+        /// A list of conditions. If one of them is not true, the skill effect will not apply.
+        /// </summary>
+        public List<Condition> Conditions = new List<Condition>();
+
+        public SkillEffect(string affectsStatName, bool ignoreSkillStrength, params Condition[] Conditions)
         {
             this.affectsStatName = affectsStatName;
             this.ignoreSkillStrength = ignoreSkillStrength;
+            this.Conditions.AddRange(Conditions);
         }
 
         public virtual object ProcessStat(Stat stat, object value, float strength, StatMachine statMachine) {
             return value;
+        }
+
+        /// <summary>
+        /// Add a condition that must be resolved true for the skill effect to apply.
+        /// </summary>
+        /// <param name="Condition">The Condition that should be added</param>
+        public SkillEffect AddCondition(Condition Condition) {
+            Conditions.Add(Condition);
+            return this;
+        }
+
+        /// <summary>
+        /// Add one or more conditions that must be resolved true for the skill effect to apply.
+        /// </summary>
+        /// <param name="Condition">The Conditions that should be added</param>
+        public SkillEffect AddConditions(params Condition[] Conditions)
+        {
+            this.Conditions.AddRange(Conditions);
+            return this;
         }
 
         /// <summary>
